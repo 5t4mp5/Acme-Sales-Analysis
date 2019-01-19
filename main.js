d2d087541fd65fb5ab37f9b5359928ee03cffe73
Dave Siegel <dave@Admins-MBP.home>
2019-01-19 03:27:04 -0500

    logging out results

--- main.js
+++ main.js
@@ -63,26 +63,37 @@ const products = [
 debugger;
 
 function productsPurchased(orders, products){
-  return products.filter(product => 
+    output = products.filter(product => 
     orders.some(order => order.productId === product.id)  
   );
+  output.forEach(prod => {
+    console.log(`Product ID: ${prod.id}
+Name: ${prod.name}
+Price: ${prod.price}
+`)
+  });
 }
 
 function topSellingProductByQuantity(orders, products){
   products.forEach(product => {
     if(!product.total) product.total = 0;
 
-    product.total += orders.reduce((acc, order) => {
-      return order.productId === product.id ? acc + order.quantity * product.price
-      : acc;
-    }, 0);
+    product.total += orders.reduce((acc, order) => 
+      order.productId === product.id ? acc + order.quantity : acc, 
+      0);
   });
 
-  return products.sort((a, b) => a.total > b.total ? -1 : 1)[0];
+  output = (products.sort((a, b) => a.total > b.total ? -1 : 1)[0]);
+  console.log(`Product ID: ${output.id}
+Name: ${output.name}
+Price: ${output.price}`)
 }
 
 function usersWithOrders(users, orders){
-  return users.filter(user => {
-    return orders.some(order => order.userId === user.id);
+  const output = users.filter(user => orders.some(order => order.userId === user.id));
+  output.forEach(user => {
+    console.log('User ID:', user.id);
+    console.log('User Name:', user.name);
+    console.log('');
   });
 }
